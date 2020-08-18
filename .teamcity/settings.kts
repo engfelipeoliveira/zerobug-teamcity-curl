f25520ade5e8bada781192cc843b317b19968013
ads14331 <felipe.silvadeoliveira@ssq.ca>
2020-08-18 15:29:50 -0400

    add files

---
+++ .teamcity/settings.kts
@@ -0,0 +1,25 @@
+package _Self.buildTypes
+
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+
+object Build : BuildType({
+    name = "Build"
+
+    vcs {
+        root(HttpsGithubComEngfelipeoliveiraZerobugTeamcityCurlGitRefsHeadsMaster)
+    }
+
+    steps {
+        script {
+            name = "curl"
+            scriptContent = """curl -d "token=5db63b48744bb86a21154f28cfd4c446&id_target=129&id=11001118" -X POST https://zerobug.co/build.php"""
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+})
