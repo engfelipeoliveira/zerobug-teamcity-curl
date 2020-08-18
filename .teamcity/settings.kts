88bddc741cfad0cf2cc4c553eb32a37b711876c2
engfelipeoliveira <cvfelipeoliveira@gmail.com>
2020-08-18 18:51:47 -0400

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,15 +1,17 @@
-package _Self.buildTypes
-
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
-object zerobugcurl : BuildType({
-    name = "zerobug curl"
+version = "2020.1"
 
+project {
+  buildType {
+    id("zerobugcurl")
+    name = "zerobug curl"
     steps {
         script {
             name = "curl"
-            scriptContent = """curl -d "token=5db63b48744bb86a21154f28cfd4c446&id_target=129&id=11001118" -X POST https://zerobug.co/build.php"""
+            scriptContent = "curl -d 'token=5db63b48744bb86a21154f28cfd4c446&id_target=129&id=11001118' -X POST https://zerobug.co/build.php"
         }
     }
-})
+  }
+}
