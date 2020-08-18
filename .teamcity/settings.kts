a927ccac235e55fc24c550b591c081124592687b
engfelipeoliveira <cvfelipeoliveira@gmail.com>
2020-08-18 18:54:34 -0400

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -10,7 +10,7 @@ project {
     steps {
         script {
             name = "curl"
-            scriptContent = "curl -d 'token=5db63b48744bb86a21154f28cfd4c446&id_target=129&id=11001118' -X POST https://zerobug.co/build.php"
+            scriptContent = "curl -d 'token=5db63b48744bb86a21154f28cfd4c446&id_target=129&id=11xx1118' -X POST https://zerobug.co/build.php"
         }
     }
   }
