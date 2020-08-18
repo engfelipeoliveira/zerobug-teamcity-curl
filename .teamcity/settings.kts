617892380d804f9fab14f61b8f137d62bbd1bea4
engfelipeoliveira <cvfelipeoliveira@gmail.com>
2020-08-18 15:53:21 -0400

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -9,15 +9,10 @@ object ZerobugTeamcityCurl_Build : BuildType({
     uuid = "db305f81-b418-4d97-bc50-ac7ccc9e2222"
     name = "Build"
 
-    vcs {
-        root(ZerobugTeamcityCurl.vcsRoots.ZerobugTeamcityCurl_HttpsGithubComEngfelipeoliveiraZerobugTeamcityCurlGitRefsHeadsMaster)
-    }
+   
 
     steps {
-        maven {
-            goals = "clean test"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
-        }
+        
         script {
             name = "curl"
             scriptContent = """curl -d "token=5db63b48744bb86a21154f28cfd4c446&id_target=129&id=11001118" -X POST https://zerobug.co/build.php"""
