59ad812cbc9113191ef60249abcbf9e00f3dfa7d
engfelipeoliveira <cvfelipeoliveira@gmail.com>
2020-08-23 12:33:31 -0400

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,17 +1,1 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
-version = "2020.1"
-
-project {
-  buildType {
-    id("zerobugcurl")
-    name = "zerobug curl"
-    steps {
-        script {
-            name = "curl"
-            scriptContent = "curl -k -u 'RTOfHaDYLUum7q8jUfDL1598117275:831ef1fc793bade6f7bcfe7d089587b7' --request POST -H 'Content-Type:application/json'  https://zerobug.co/ScanTarget/"
-        }
-    }
-  }
-}
